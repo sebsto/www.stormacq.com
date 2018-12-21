0f2edcce189ab39b2c5fe01b5d4f7c27f720f76d
Stormacq, Sebastien <stormacq@amazon.lu>
2018-12-21 19:10:55 +0000

    add infrastructure and bundle

--- publish.sh
+++ publish.sh
@@ -3,4 +3,4 @@
 AWS_PROFILE=seb
 BUCKET_NAME=stormacq.com
 
-jekyll build --incremental && aws --profile $AWS_PROFILE s3 sync _site/ s3://$BUCKET_NAME/
+bundle exec jekyll build --incremental && aws --profile $AWS_PROFILE s3 sync _site/ s3://$BUCKET_NAME/
