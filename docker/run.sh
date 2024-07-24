31b92182f4f6c38dafb65a2f3b1c4e0bde1c23d9
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-07-24 08:06:32 +0200

    update to Jekyll 4

--- docker/run.sh
+++ docker/run.sh
@@ -5,13 +5,15 @@
 
 # image from https://github.com/envygeeks/jekyll-docker
 
-export JEKYLL_VERSION=3.8
+# export JEKYLL_VERSION=3.8
 # export JEKYLL_VERSION=4.0
-docker run \
-  -p 4000:4000 \
-  --rm \
-  --volume="$PWD:/srv/jekyll" \
-  --volume="$PWD/vendor/bundle:/usr/local/bundle" \
-  -it jekyll/jekyll:$JEKYLL_VERSION \
-  jekyll serve -H 0.0.0.0 -P 4000 
+# docker run \
+#   -p 4000:4000 \
+#   --rm \
+#   --volume="$PWD:/srv/jekyll" \
+#   --volume="$PWD/vendor/bundle:/usr/local/bundle" \
+#   -it jekyll/jekyll:$JEKYLL_VERSION \
+#   jekyll serve -H 0.0.0.0 -P 4000 
 #   jekyll build --watch 
+
+bundle exec jekyll serve -H 0.0.0.0 -P 4000
