1141c707193e997d5f71c65f38d189edb8779dcc
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2026-02-20 08:02:11 +0100

    add container files to test locally

---
+++ docker/run-container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run Jekyll site using Apple Container
+# Run from the main site directory: ./docker/run-container.sh
+
+IMAGE_NAME="jekyll-site"
+
+# Build the image with the Gemfile from the project root
+# container build -t $IMAGE_NAME -f docker/Dockerfile .
+
+# Run with the site mounted so changes are picked up
+container run \
+  -p 4000:4000 \
+  --rm \
+  --volume="$PWD:/srv/jekyll" \
+  -it $IMAGE_NAME
