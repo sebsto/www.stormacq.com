473ccdde25254f2173a0a5e36d454a381bf8fde4
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2026-03-04 12:15:13 +0100

    add finch launch script

---
+++ docker/run-finch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Run Hugo site using Apple Container
+# Run from the main site directory: ./docker/run-container.sh
+
+IMAGE_NAME="hugo-site"
+
+# Build the image
+# finch build -t $IMAGE_NAME -f docker/Dockerfile .
+
+# Run with the site mounted so changes are picked up
+finch run \
+  -p 1313:1313 \
+  --rm \
+  --volume="$PWD:/src" \
+  -it $IMAGE_NAME \
+  hugo server --bind 0.0.0.0 -p 1313 --watch --poll 700ms
